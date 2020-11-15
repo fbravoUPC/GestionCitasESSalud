718f451bc70d5a6f2b52a99987c650f12460a41d
Freddy Alberto Bravo Angulo <u201300062@upc.edu.pe>
2020-11-15 02:14:32 -0500

    Avance8

---
+++ app/src/main/java/com/fbravo/gestioncitasessalud/MenuCitas.java
@@ -0,0 +1,14 @@
+package com.fbravo.gestioncitasessalud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuCitas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_citas);
+    }
+}
