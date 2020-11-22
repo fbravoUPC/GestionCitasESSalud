49fd13c2f6de1a8e77ea7a930f884fdc35ce9fe3
Freddy Alberto Bravo Angulo <u201300062@upc.edu.pe>
2020-11-22 12:10:27 -0500

    Avance12

--- app/src/main/java/com/fbravo/gestioncitasessalud/CitasProgramadas.java
+++ app/src/main/java/com/fbravo/gestioncitasessalud/CitasProgramadas.java
@@ -3,12 +3,25 @@ package com.fbravo.gestioncitasessalud;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CitasProgramadas extends AppCompatActivity {
-
+    TextView NombreUsuario,DNIusuario,EdadUsuario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_citas_programadas);
+        asignarReferencias();
+    }
+
+    private void asignarReferencias() {
+        NombreUsuario = findViewById(R.id.NombreUserCitaPro);
+        DNIusuario = findViewById(R.id.DNIUserCitaPro);
+        EdadUsuario =findViewById(R.id.EdadUserCitaPro);
+        NombreUsuario.setText(MainActivity.name+" "+MainActivity.lastnamep +" "+MainActivity.lastnamem);
+        DNIusuario.setText(MainActivity.dni);
+        EdadUsuario.setText(MenuCitas.edad +" años");
+
+
     }
 }
