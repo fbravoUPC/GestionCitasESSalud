e7a6032ab06dce1509238d686fe0c0ad8cab67a7
LEONARDOLMB <64390057+LEONARDOLMB@users.noreply.github.com>
2020-11-21 22:39:02 -0500

    Citas!

--- app/src/main/java/com/fbravo/gestioncitasessalud/UserList.java
+++ app/src/main/java/com/fbravo/gestioncitasessalud/UserList.java
@@ -9,6 +9,6 @@ public class UserList extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_list);
+        setContentView(R.layout.activity_main);
     }
 }
