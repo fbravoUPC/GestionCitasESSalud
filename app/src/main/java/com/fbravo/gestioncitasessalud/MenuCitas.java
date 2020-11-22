a1b34d113a952db3d9f9f7e1d27663ca9e02f81d
LEONARDOLMB <64390057+LEONARDOLMB@users.noreply.github.com>
2020-11-22 11:05:12 -0500

    Citas!

--- app/src/main/java/com/fbravo/gestioncitasessalud/MenuCitas.java
+++ app/src/main/java/com/fbravo/gestioncitasessalud/MenuCitas.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
@@ -14,7 +15,7 @@ import java.time.temporal.ChronoUnit;
 public class MenuCitas extends AppCompatActivity {
 
     Button btn_ginecologia;
-
+    TextView NombreUsuario,DNIusuario,EdadUsuario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,16 +23,29 @@ public class MenuCitas extends AppCompatActivity {
 
         //int años = Integer.parseInt(MainActivity.fnac);
         //int edad = 2020 - años;
+        asignarReferencias();
         btn_ginecologia = findViewById(R.id.btn_ginecologia);
         btn_ginecologia.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MenuOptions.cita.setId_especialidad(2);
+               // MenuOptions.cita.setId_especialidad(2);
                 openActivity2();
 
             }
         });
     }
+
+    private void asignarReferencias() {
+        NombreUsuario = findViewById(R.id.EntradaUserCitas);
+        DNIusuario = findViewById(R.id.DNIUserCitas);
+        EdadUsuario =findViewById(R.id.EdadUserCitas);
+        NombreUsuario.setText("Bienvenido " + MainActivity.name+" "+MainActivity.lastnamep +" "+MainActivity.lastnamem);
+        DNIusuario.setText(MainActivity.dni);
+        btn_ginecologia=findViewById(R.id.btn_ginecologia);
+
+
+    }
+
     public void openActivity2() {
         Intent intent = new Intent(this, UbicacionSede.class);
         startActivity(intent);
